33a574df38deb3883bffdbfef42712e2320b4bb6
kitsunereyna <begimbetova.dana@gmail.com>
2022-02-04 20:13:49 +0600

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-"use strict";
+const search_input = document.getElementById("input");
+const search_button = document.getElementById("search");
+const panels = document.querySelectorAll(".panel");
 
-console.log("Я учу JS")
+
+search_button.addEventListener("click", search_name);
+
+function search_name(){
+    const value = search_input.value;
+    for (let index = 0; index < panels.length; index++) {
+        const song = panels[index].querySelector(".name");
+        const song_name = song.innerHTML;
+        const song_parent = song.parentElement.parentElement.parentElement;
+        if (value != song_name){
+            song_parent.style.display = "none";
+        }
+    }
+}
+
